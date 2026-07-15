f658e105ab295ab5de08406cda60d44b9b72ba69
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 16:33:43 +0600

    js keyword variable name , naming convention

---
+++ js/Module-7.6.js
@@ -0,0 +1,34 @@
+// todo:   Module no - 7.6         topic: JavaScript Keyword, Variable Name, Naming Convention.
+
+console.log('javaScript keyword : var let const ');
+let car = 'BMW';
+
+/**
+ * আমাদের variable naming :এর ক্ষেত্রে reserved যে keyword রোয়েছে সেগুলো use kora jabe na. 
+ * 
+ * reserved keyword : 
+ * let
+ * const
+ * var
+ * if
+ * else
+ * for
+ * while
+ * function
+ * return
+ * class
+ * 
+ * 
+ */
+
+//* variable name: 
+ const firstName = 'Md Ashraful islam'; // camel case : 
+ const Fullname  = 'Bangladesh';
+ const lastName= 'islam';
+
+//  variable naming convention : meaningful and consistent name: 
+ const myFullName= 'Md Ashraful Islam ';
+ const userAge = 20;
+
+
+
