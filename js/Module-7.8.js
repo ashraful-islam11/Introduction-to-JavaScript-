cfe8baea58c6a3b4f3317a227b2d3367a17bd85b
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 18:12:22 +0600

    Introduction to arithmetic operators

---
+++ js/Module-7.8.js
@@ -0,0 +1,43 @@
+// todo:   Module no -7.8      topic:Introduction to arithmetic operators. 
+
+/**
+ **  Arithmetic operators:
+ *
+ **  Addition :          + 
+ **  Subtraction :       - 
+ **  Multiplication:     * 
+ **  Division  :         /  
+ **  Modulus :           %
+ */
+
+//  addition: + 
+const waterPrise = 10;
+const MojoPrise  = 20;
+const totalPrise= waterPrise + MojoPrise;
+console.log(totalPrise);
+
+// Subtraction: 
+const headPhonePrise = 950;
+const adupterPrise = 150;
+
+const devicePrise = headPhonePrise - adupterPrise;
+console.log(devicePrise);
+
+// Multiplication: 
+const mousePrise = 10;
+const keyboardPrise = 5;
+const totalBuy= mousePrise * keyboardPrise;
+console.log(totalBuy);
+
+// Division:
+
+const totalMoney = 50;
+const totalProduct = 5;
+const everySingleProductPrise = totalMoney / totalProduct;
+console.log(everySingleProductPrise);
+
+// Modulus: 
+const bottlePrise = 32;
+const number =3;
+const modulus = bottlePrise % number;
+console.log(modulus);
