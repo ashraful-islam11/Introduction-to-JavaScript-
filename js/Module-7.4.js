3860fe7db4f151845261a15404c4d882f3b179e3
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 11:49:26 +0600

    variable animated video

---
+++ js/Module-7.4.js
@@ -0,0 +1,19 @@
+// todo: Module no - 7.4           topic: Variable Animated Video .Animated
+
+
+var box = 'blue car'; //declare the var variable 
+var boxs = 'blue box, and green box'; // redeclare the var variable .
+
+changeBox = 'green car' ; // var is reassign variable .
+
+
+
+// let variable : 
+let prise = '200 taka'; // declare 
+// let prise = '20taka'; // let is impossible redeclared.
+   prises = 300; // let is reassigned.
+
+const country ='bangladesh'; // const is only declare and no change this value ;
+
+
+console.log(box,boxs, prise, prises,country);
