8701dd254c3e6f107e5ecbfbe44a381aa9426fe2
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 20:35:39 +0600

    module summery

---
+++ js/Module-7.10.js
@@ -0,0 +1,14 @@
+// todo:     Module no - 7.10      topic: Module summery 
+/**
+ *  javaScript history 
+ *  how to declare variable. 
+ *  variable types .
+ *  
+ *  keywords . and reserved keyword 
+ *  math needed to start learning programming .
+ *  let const and var use case.
+ * 
+ *  Arithmetic operator 
+ *  mathematical operation shorthand. 
+ 
+ */
