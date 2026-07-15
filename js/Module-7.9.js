5afc217b5061b46b8a36a3b02c49c35e454d2005
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 19:29:23 +0600

    (Advanced) Mathematical Operation Shorthand

---
+++ js/Module-7.9.js
@@ -0,0 +1,94 @@
+// todo:   Module no - 7.9         topic: (Advanced) Mathematical Operation Shorthand
+
+
+//  addition: + 
+const waterPrise = 10;
+const MojoPrise  = 20;
+const totalPrise= waterPrise + MojoPrise;
+console.log(totalPrise);
+
+// Subtraction: 
+const headPhonePrise = 950;
+const adupterPrise = 150;
+
+const devicePrise = headPhonePrise - adupterPrise;
+console.log(devicePrise);
+
+// Multiplication: 
+const mousePrise = 10;
+const keyboardPrise = 5;
+const totalBuy= mousePrise * keyboardPrise;
+console.log(totalBuy);
+
+// Division:
+
+const totalMoney = 50;
+const totalProduct = 5;
+const everySingleProductPrise = totalMoney / totalProduct;
+console.log(everySingleProductPrise);
+
+// Modulus: 
+const bottlePrise = 32;
+const number =3;
+const modulus = bottlePrise % number;
+console.log(modulus);
+
+
+// advanced topic: 
+
+let pantPrise = 500;
+    pantPrise =400;
+    console.log(pantPrise);
+
+    // short cut : sum 
+    pantPrise = pantPrise +200;
+    console.log(pantPrise);
+
+    // short cut : sum 
+    pantPrise += 200;
+    console.log(pantPrise);
+
+    // short cut : Subtraction :
+
+    pantPrise = pantPrise - 100;
+    console.log(pantPrise);
+
+    // short cut : Subtraction
+    pantPrise -= 100;
+    console.log(pantPrise);
+
+    // short cut : multiply:
+    pantPrise = pantPrise * 5 ;
+    console.log(pantPrise);
+
+    // short cut : multiply :
+    pantPrise *= 2;
+    console.log(pantPrise);
+
+    // short cut : multiply :
+    pantPrise = pantPrise / 2;
+    console.log(pantPrise);
+
+    // short cut : division 
+    pantPrise /= 2;
+    console.log(pantPrise);
+
+
+    // ++ operator : 
+    pantPrise++; 
+    pantPrise++;
+    pantPrise++; 
+    pantPrise++;
+    pantPrise++;
+    console.log(pantPrise);
+
+    // --operator: 
+    pantPrise--;
+    pantPrise--;
+    pantPrise--;
+    pantPrise--;
+    pantPrise--;
+    console.log(pantPrise);
+
+
+
