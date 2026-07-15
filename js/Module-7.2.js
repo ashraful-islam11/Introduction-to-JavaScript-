ae31a7d0ebf671aa1eeebb92f9e8342de65eb9d9
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-15 06:42:26 +0600

    Maths Needed to start Learning Programming Language

---
+++ js/Module-7.2.js
@@ -0,0 +1,19 @@
+//  todo:          Module no - 7.2             topic:       Maths Needed to Start Learning Programming Language
+
+/**
+ ** Maths Needed to Start Learning Programming Language:
+ *
+ * 1. Number: [+ve , -ve , 0 ]
+ * 2, Fraction:  0.5 , 24.4 
+ * 3. Even, Odd - 10, 9 
+ * 4. Remainder: after dividing a number 
+ * 5. Basic Math Operation : add , deduct ,multiply, divide 
+ * 6. Percentage : % Modulus operator. 
+ * 7. Sum + .
+ * 8. Avg: 10, 20, 30 = { 10+ 20+ 30 =60/3= 20 avg: 20 }
+ * 9. Max / min , 23,44,66,44, { max= 66 and min = 23}
+ * 10.series : 1-10 .
+ * 11.calculation : Area of Triangle - 1.5 *base * height 
+ * 12. share profit { 7=100 , 3?    100/7*3 }
+
+ */
